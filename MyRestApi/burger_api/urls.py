70507438e6907a4763f68370517e75c121085345
Sifur Rahaman <saifurrshishir@gmail.com>
2021-01-11 22:59:08 +0600

    JWT token added

--- MyRestApi/burger_api/urls.py
+++ MyRestApi/burger_api/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from rest_framework.routers import DefaultRouter
 from burger_api.views import UserProfileViewSet
 
 router = DefaultRouter()
 router.register(r"user", UserProfileViewSet)
 
-urlpatterns = [] + router.urls
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+] + router.urls
